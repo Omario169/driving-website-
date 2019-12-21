a6d4721407f4ef0217cac2b85460ae611aaf12b9
Omario169 <47423327+Omario169@users.noreply.github.com>
2019-12-21 19:09:21 +0000

    diamond certification added

--- js/active.js
+++ js/active.js
@@ -21,7 +21,7 @@ document.getElementById("main-banner").onmouseleave = function() {
         bannerLoop();
     }, bannerTimer);
 }
-
+//initialise new function here 
 document.getElementById("imgbanbtn-prev").onclick = function() {
     if (bannerStatus === 1) {
         bannerStatus = 2;
@@ -32,6 +32,7 @@ document.getElementById("imgbanbtn-prev").onclick = function() {
     else if (bannerStatus === 3) {
         bannerStatus = 1;
     }
+    bannerLoopRev();
     bannerLoop();
 }
 
@@ -92,6 +93,61 @@ function bannerLoop() {
              document.getElementById("imgban1").style.opacity = "1";
             }, 1000);
         bannerStatus = 1;
+    }
+    
+}
+
+function bannerLoopRev() {
+    if (bannerStatus === 1) {
+        document.getElementById("imgban3").style.opacity = "0";
+
+        setTimeout(function() {
+            document.getElementById("imgban3").style.left = "0px";
+            document.getElementById("imgban3").style.zIndex = "1000";
+            document.getElementById("imgban2").style.left = "-1200px";
+            document.getElementById("imgban2").style.zIndex = "1500";
+            document.getElementById("imgban1").style.left = "1200px";
+            document.getElementById("imgban1").style.zIndex = "500";
+        }, 500);
+        setTimeout(function() {
+
+             document.getElementById("imgban3").style.opacity = "1";
+            }, 1000);
+        bannerStatus = 3;
+    }
+    else if (bannerStatus === 2) {
+        document.getElementById("imgban1").style.opacity = "0";
+
+        setTimeout(function() {
+            document.getElementById("imgban1").style.left = "0px";
+            document.getElementById("imgban1").style.zIndex = "1000";
+            document.getElementById("imgban2").style.left = "-1200px";
+            document.getElementById("imgban2").style.zIndex = "1500";
+            document.getElementById("imgban3").style.left = "1200px";
+            document.getElementById("imgban3").style.zIndex = "500";
+        }, 500);
+        setTimeout(function() {
+
+             document.getElementById("imgban1").style.opacity = "1";
+            }, 1000);
+        bannerStatus = 1;
+    }
+    else if (bannerStatus === 3) {
+        document.getElementById("imgban2").style.opacity = "0";
+
+        setTimeout(function() {
+            document.getElementById("imgban2").style.left= "0px";
+            document.getElementById("imgban2").style.zIndex = "1000";
+            document.getElementById("imgban1").style.left = "-1200px";
+            document.getElementById("imgban1").style.zIndex = "1500";
+            document.getElementById("imgban3").style.left = "1200px";
+            document.getElementById("imgban3").style.zIndex = "500";
+        }, 500);
+        setTimeout(function() {
+
+             document.getElementById("imgban2").style.opacity = "1";
+            }, 1000);
+        bannerStatus = 2;
     }
     
 }
