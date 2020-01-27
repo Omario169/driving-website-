4133bd09372ac5dca71ffa282ec58bc626dad206
Omar Faruk <ofaruk0169@gmail.com>
2020-01-27 04:29:58 +0000

    An improved naviagation bar has been added

--- js/active.js
+++ js/active.js
@@ -94,3 +94,21 @@ function playPauseSlides() {
         playPauseBtn.style.backgroundPositionY="-33px"
     }
 }
+
+function showMenu() {
+    var b = document.getElementById("bar");
+    var t = document.getElementById("times");
+    var m = document.getElementById("mob-menu");
+    b.style.display = "none";
+    t.style.display = "block";
+    m.style.display = "block";
+  }
+  
+  function closeMenu() {
+    var b = document.getElementById("bar");
+    var t = document.getElementById("times");
+    var m = document.getElementById("mob-menu");
+    b.style.display = "block";
+    t.style.display = "none";
+    m.style.display = "none";
+  }
